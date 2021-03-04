f924cd6bc674042453e7dc0b6d55c2075fb85f80
mokusmellkas <73641320+mokusmellkas@users.noreply.github.com>
2021-03-04 13:34:46 +0100

    Add files via upload

--- frontend/reactfront/src/App.js
+++ frontend/reactfront/src/App.js
@@ -1,78 +1,80 @@
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 import './App.css';
-import React from 'react'
+import React from "react";
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState();
 
-const [username, setUsername] = useState("")
-const [cityname, setCityname] = useState("")
-const [streetname, setStreetname] = useState("")
-const [housenumber, sethousenumber] = useState("")
-const [file, setFile] = useState([])
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
 
-  console.log("Client is running"); 
+  const handleSubmission = () => {
+    const formData = new FormData();
 
+        let userData = {};
+        
+        const ls = document.querySelectorAll('.toJson');
 
- function onClickHandler (e) {
-   e.preventDefault();
-  let username = document.getElementById('textInput').value;
-  let cityname = document.getElementById('cityInput').value;
-  let streetname = document.getElementById('streetInput').value;
-  let housenumber = document.getElementById('housenumberInput').value;
-  let file = document.getElementById('filenameInput');
-  let formData = new FormData(); 
-  
-  formData.append("username", username );
-  formData.append("cityname", cityname );
-  formData.append("streetname", streetname );
-  formData.append("housenumber", housenumber );
-  formData.append('userfile', file.files[0]);
-  
-  console.log(file.files);
-
-  fetch('http://127.0.0.1:8000/upload', {
-    method: 'POST',
-    body: formData
-  })
-    .then(result => {
-      console.log('Success:', result);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-
-
-  for (const iterator of formData) {
-      console.log(iterator)
+   for (const l of ls) {
+      userData[l.name] = l.value;
     }
+    userData.selectedFile = selectedFile.name;
+        
+        console.log(selectedFile.name);
+        console.log(userData);
+
+        formData.append("userFile", selectedFile);
+        formData.append("userData", JSON.stringify(userData));
+
+fetch('http://127.0.0.1:8000/upload', {
+  method: 'POST',
+  body: formData
+})
+.then(result => {
+  console.log('Success:', result);
+})
+.catch(error => {
+  console.error('Error:', error);
+});
+
+
+for (const iterator of formData) {
+  console.log(iterator)
+}
 
 } 
 
-  
-  return (
-    <div className="App">
+
+return (
+  <div className="App">
 <form id='uploadForm'>
+  <h1>Join US</h1>
   <div className="simpleform">
   <label htmlFor="textInput">Név: </label>
-  <input id="textInput" type="text" onChange={setUsername}/>
+  <input className="toJson" id="textInput" type="text" name="name" />
   </div>
   <div className="simpleform">
   <label htmlFor="cityInput">Város: </label>
-  <input id="cityInput" type="name" onChange={setCityname}/>
+  <input className="toJson" id="cityInput" type="text" name="city" />
   </div>
   <div className="simpleform">
   <label htmlFor="streetInput">Utca: </label>
-  <input id="streetInput" type="name" onChange={setStreetname}/>
+  <input className="toJson" id="streetInput" type="text" name="street" />
   </div>
   <div className="simpleform">
   <label htmlFor="housenumberInput">Házszám: </label>
-  <input id="housenumberInput" type="name" onChange={sethousenumber}/>
+  <input className="toJson" id="housenumberInput" type="text" name="housenumber" />
+  </div>
+  <div className="simpleform">
+  <label htmlFor="streetInput">Email: </label>
+  <input className="toJson" type="email" name="email" />
   </div>
 
   <label htmlFor="filenameInput">---Fájlnév: </label>
-  <input id="filenameInput" type="file" name="sampleFile" /> 
-  <input type='submit' onClick={onClickHandler} value='Upload!'/> 
+  <input id="filenameInput" type="file" name="file" onChange={changeHandler}/> 
 </form> 
+  <button type='submit' onClick={handleSubmission} value='Upload!'> Kattints </button>
     {/*   <input type="file" name="file" onChange={onChangeHandler} /> */}
     </div>
   );
